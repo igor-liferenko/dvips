c74ada01c22bc22938c12041c78a8fc7d9d79839
Igor Liferenko <igor.liferenko@gmail.com>
2020-04-08 18:00:14 +0700

    edit

--- output.c
+++ output.c
@@ -176,7 +176,24 @@ copyfile_general(const char *s, struct header_list *cur_header)
       }
 #endif /* VMCMS */
 #else /* VMCMS || MVSXA */
-      sprintf(errbuf, "Could not find figure file %.500s; continuing.", s);
+        char utf8file[10000];
+        char *utf8fileptr = utf8file;
+        wchar_t xchr[256];
+        #include "/home/user/ctex/mapping"
+        for (const char *fileptr=s; *fileptr!='\0'; fileptr++) {
+             if ((unsigned char) *fileptr <= 127) {
+               *utf8fileptr++ = *fileptr;
+             }
+             else {
+               char mb[MB_CUR_MAX];
+               int len = wctomb(mb, xchr[(unsigned char) *fileptr]);
+               for (int i = 0; i < len; i++)
+                 *utf8fileptr++ = mb[i];
+             }
+        }
+        *utf8fileptr = '\0';
+
+      sprintf(errbuf, "Could not find figure file %s; continuing.", utf8fileptr);
       if (secure == 2) {
          strcat(errbuf, "\nNote that an absolute path or a relative path with .. are denied in -R2 mode.");
       }
